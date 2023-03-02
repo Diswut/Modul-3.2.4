f6bb02d66c8b6d2331c759386857b30134bb92ad
Diswut <diswut@gmail.com>
2023-03-02 19:47:02 +0300

    Добавьте файлы проекта.

---
+++ Modul 3.2.4/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string myName;
+        myName = "Mike";
+
+        Console.WriteLine(myName);
+
+        Console.ReadKey();
+
+    }
+}
