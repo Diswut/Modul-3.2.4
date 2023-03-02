fe88dd816fd5e1747ecdc33e49a55eaa75b23896
Diswut <diswut@gmail.com>
2023-03-02 22:24:52 +0300

    Обнова на модуле 3.3.1

--- Modul 3.2.4/Program.cs
+++ Modul 3.2.4/Program.cs
@@ -2,11 +2,16 @@
 {
     private static void Main(string[] args)
     {
-        string myName = "Mike";
+        string MyName = "Mike";
+        byte MyAge = 27;
+        bool HaveIApet = true;
+        double MyShoeSize = 37.5;
 
-        Console.WriteLine(myName);
+        Console.WriteLine("My name is " + MyName);
+        Console.WriteLine("MyAge " + MyAge);
+        Console.WriteLine("Do I have a pet? " + HaveIApet);
+        Console.WriteLine("My shoe size is " + MyShoeSize);
 
-        Console.ReadKey();
 
     }
 }
