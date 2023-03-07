804aeab003eb641d07c5b16a3574278300fed53e
Diswut <diswut@gmail.com>
2023-03-07 18:03:48 +0300

    Сделали задание про перечисления Enum

---
+++ Enum/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DaysOfWeek MyFavoritDay = DaysOfWeek.Friday;
+
+
+        Console.WriteLine(MyFavoritDay);
+        Console.WriteLine(Semaphore.Green);
+        double result = 10.0 * 2.0 / 5.0;
+       
+    }
+        
+        enum DaysOfWeek : byte
+        {
+
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday,
+        }
+    enum Semaphore
+    {
+        Red = 100,
+        Yellow = 200,
+        Green = 300,
+    }
+
+
+}
--- Modul 3.2.4/Program.cs
+++ Modul 3.2.4/Program.cs
@@ -1,5 +1,7 @@
 ﻿internal class Program
 {
+    
+
     private static void Main(string[] args)
     {
         string MyName = "Mike";
@@ -7,11 +9,14 @@
         bool HaveIApet = true;
         double MyShoeSize = 37.5;
 
-        Console.WriteLine("My name is " + MyName);
+        Console.WriteLine($"My name is   {MyName}");
         Console.WriteLine("MyAge " + MyAge);
         Console.WriteLine("Do I have a pet? " + HaveIApet);
         Console.WriteLine("My shoe size is " + MyShoeSize);
-
+        Console.WriteLine("\u00A4");
+        
 
     }
+
+
 }
