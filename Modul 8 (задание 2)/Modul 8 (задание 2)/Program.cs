a5445256dae57226278a00a8a1a1d0313630c5b8
Diswut <diswut@gmail.com>
2023-04-18 22:01:12 +0300

    Модуль 8 задание № 2

---
+++ Modul 8 (задание 2)/Modul 8 (задание 2)/Program.cs
@@ -0,0 +1,60 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string path = @"D:\foto";
+        long i = 0;
+        i = GetDirSize(path);
+        Console.WriteLine(i);
+    }
+    public static long GetDirSize(string path)
+    {
+        DirectoryInfo directoryInfo;
+
+        if (Directory.Exists(path))
+        {
+            try
+            {
+                directoryInfo = new DirectoryInfo(path);
+                
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Не верный путь к папке");
+                return 0;
+            }
+
+            long size = 0;
+
+            try
+            {
+                foreach (var item in directoryInfo.GetFiles())
+                {
+                    size += item.Length;
+                }
+            }
+            catch (Exception) { }
+
+            try
+            {
+
+                foreach (DirectoryInfo item in directoryInfo.GetDirectories())
+                {
+                    size += GetDirSize(item.FullName);
+                }
+
+            }
+            catch (Exception) { }
+            return size;
+
+        }
+        else
+        {
+            Console.WriteLine("[!] такой каталог не найден");
+            return 0;
+        }
+        
+    }
+
+
+}
