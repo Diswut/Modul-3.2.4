725054181d44b7a473a54955d7133dc94b3d1724
Toper <toptopkz@hotmail.com>
2023-05-03 22:36:47 +0300

    Модуль 9

---
+++ Modul 9/Modul 9/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Data;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography.X509Certificates;
+namespace Modul_9;
+
+internal class Program
+{
+    #region Задание 1
+
+    static void Main(string[] args)
+    {
+        Exception[] exceptions = new Exception[5];
+        exceptions[0] = new DivideByZeroException();
+        exceptions[1] = new AggregateException();
+        exceptions[2] = new MyException();
+        exceptions[3] = new ApplicationException();
+        exceptions[4] = new InvalidOperationException();
+        
+        foreach (var exception in exceptions)
+        {
+            try
+            {
+                throw exception;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+        }
+        #endregion
+
+        #region Задание 2
+        List<string> list = new List<string> 
+        {
+            "Васин",
+            "Дуров",
+            "Медведев",
+            "Шнуров",
+            "Ионов",
+        };
+
+        list.Sort();
+        foreach (var exception in list) { Console.WriteLine(exception); }
+        list.Reverse();
+        foreach (var exception in list) { Console.WriteLine(exception); }
+
+    }
+
+        #endregion
+
+}
